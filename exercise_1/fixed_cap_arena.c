c6f4422a9e5cdbb34fb7fc31c297208ea5bde6a3
PLAZMAMA <maorioriori@gmail.com>
2024-09-23 21:47:31 -0400

    move arena_pop, arena_clear skeleton funcs

--- exercise_1/fixed_cap_arena.c
+++ exercise_1/fixed_cap_arena.c
@@ -26,13 +26,14 @@ void *arena_push(Arena *arena, uint64_t size) {
     return result_head;
 }
 
+void arena_pop(Arena *arena, uint64_t size);
+void arena_clear(Arena *arena);
+
 void arena_set_auto_align(Arena *arena, uint64_t align);
 unsigned int arena_pos(Arena *arena);
 void *arena_push_no_zero(Arena *arena, uint64_t size);
 void *arena_push_aligner(Arena *arena, uint64_t alignment);
 void arena_pop_to(Arena *arena, uint64_t pos);
-void arena_pop(Arena *arena, uint64_t size);
-void arena_clear(Arena *arena);
 
 int main() {
 }
